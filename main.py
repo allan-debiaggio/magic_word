c37d6710c739a6e39d673cda9c4ae17d6bfde6fa
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-04-27 18:35:40 +0200

    One window added with Threads

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ Implement a program that :
 - When the right keys are pressed, the end of the clip (Samuel Jackson saying "PLEASE ! GODDAMMIT, I HATE THIS HACKER CRAP !")
 """
 
-import tkinter
+import customtkinter
 import keyboard
 import vlc
 import time
 import pygame
+import threading
 
 pygame.mixer.init()
 
@@ -32,23 +33,64 @@ def error_sound() :
     # pygame.mixer.music.play(1)
 
 
-def looping_sound() :
-    pygame.mixer.music.load("assets/magic_word.mp3")
-    pygame.mixer.music.play(-1)
-    
 
-
-
-def main() :
+def background_task() :
     while True :
         if keyboard.read_event() :
             error_sound()
             looping_sound()
             time.sleep(0.1)
+
         if keyboard.is_pressed("ctrl+n+o") :
             pygame.mixer.music.stop()
             closing_sound()
             break
+
+
+
+
+def looping_sound() :
+    pygame.mixer.music.load("assets/magic_word.mp3")
+    pygame.mixer.music.play(-1)
+    
+def on_close(event = None) :
+    print("Close button pressed !")
+
+def on_minimize(event = None) :
+    print("Minimized button pressed !")
+
+def on_restore(event = None) :
+    print("Restore pressed !")
+
+def create_window() :
+    window = customtkinter.CTk()
+    window.title("Ah ah aaaaah !")
+    window.geometry("300x150")
+
+    label = customtkinter.CTkLabel(
+        window,
+        text = "You didn't say the magic word !",
+        font = ("Arial", 14)
+    )
+    label.pack(pady = 20)
+
+    window.protocol("WM_DELETE_WINDOW", on_close) # Action when closing button is pressed
+
+    window.bind("<Unmap>", on_minimize) 
+    window.bind("<Map>", on_restore)
+
+    return window
+
+
+
+def main() :
+    window = create_window()
+
+    thread = threading.Thread(target=background_task,daemon=True)
+    thread.start()
+
+    window.mainloop()
+
 
 
 main()
